3e2597ba643c322218da736412e240e2c34ee45e
reaid44 <reaidhasanmd88@gmail.com>
2025-12-16 01:23:27 +0600

    Fix app.run call to use correct port

--- app.py
+++ app.py
@@ -99,5 +99,4 @@ def download():
 if __name__ == "__main__":
     auto_update_ytdlp()
     print("✅ Flask server running at: http://127.0.0.1:5000")
-0")
-    app.run(debug=True, host="0.0.0.0", port=5
+    app.run(debug=True, host="0.0.0.0", port=5000)
